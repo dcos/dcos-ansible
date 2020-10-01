303d966c45c3964da6dcdaf4b46fc23fe485623b
Jan Ulferts <julferts@mesosphere.com>
2020-10-01 07:44:58 +0200

    fix: DC/OS expects cuda 10

--- molecule/ec2_gpu/tests/test_default.py
+++ molecule/ec2_gpu/tests/test_default.py
@@ -9,3 +9,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_nvidia_smi(host):
     cmd = host.run("sudo nvidia-smi -L")
     assert 'GPU 0' in cmd.stdout
+
+def test_cuda10_caps(host):
+    nvidia_caps = host.file("/dev/nvidia-caps")
+
+    assert True == nvidia_caps.exists
+    assert False == nvidia_caps.is_directory
